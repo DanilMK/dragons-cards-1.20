df490b4c92c7f45126c631c7771aacbdca8c3f1a
Danil_MK <danilmaksimovih@gmail.com>
2024-03-15 20:22:04 +0300

    Project create

---
+++ src/client/java/net/smok/DragonsCardsClient.java
@@ -0,0 +1,10 @@
+package net.smok;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DragonsCardsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/net/smok/DragonsCards.java
@@ -0,0 +1,16 @@
+package net.smok;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DragonsCards implements ModInitializer {
+	private static final String MOD_ID = "dragons-cards";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
